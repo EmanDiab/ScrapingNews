e7ae416656281d8b6a44cdf2aa3eaf07a874a02f
EmanDiab <eman.diab5010@gmail.com>
2021-05-24 00:34:22 +0200

    Scraping news and articles

--- gemotask/spiders/newspider.py
+++ gemotask/spiders/newspider.py
@@ -6,31 +6,27 @@ class newspider(scrapy.Spider):
     start_urls = ["https://www.bbc.com/"]
 
     def parse(self, response):
-        links = []
-        for news in response.css("div.media__content"):
-            print(news)
+        xp = "//div[@class='media__content']//a[@class='media__link']/@href"
+        return (scrapy.Request(url, callback=self.parseArticle) if url.startswith("htt") else scrapy.Request("https://www.bbc.com/" +url , callback=self.parseArticle)  for url in response.xpath(xp).extract())
 
-            try:
-                yield {
-                    "title": news.css("a.media__link::text").get().strip().replace("\n", ""),
-                    "link": "https://www.bbc.com" + news.css("a.media__link").attrib["href"],
-                    "": links.append("https://www.bbc.com" + news.css("a.media__link").attrib["href"])
-                }
-            except:
-                yield {
-                    "title": None,
-                    "link": None
-                }
-        for link in links:
-            next_page = scrapy.Request(link)
-            if next_page is not None:
-                yield response.follow(response.urljoin(link), callback=self.parseArticle)
 
     def parseArticle(self, response):
 
         if response is not None:
-            yield {
-                "article_text": "\n".join(
-                    response.xpath('//div[@class="ssrcss-18snukc-RichTextContainer e5tfeyi1"]/p/text()').extract())
-
-            }
+            try:
+                yield {
+                    "title": response.css("h1::text").get(),
+                    "link": response.xpath("//meta[@property='og:url']/@content").get(),
+                    "author": response.xpath(
+                        "//a[@class='author-unit__text b-font-family-serif']/text()").get().replace("By ", ""),
+                    "article_text": "\n".join(
+                        response.xpath('//div[@class="ssrcss-18snukc-RichTextContainer e5tfeyi1"]/p/text()').extract())
+                }
+            except:
+                yield {
+                    "title": response.css("h1::text").get(),
+                    "link": response.xpath("//meta[@property='og:url']/@content").get(),
+                    "author": None,
+                    "article_text": "\n".join(
+                        response.xpath('//div[@class="ssrcss-18snukc-RichTextContainer e5tfeyi1"]/p/text()').extract())
+                }
